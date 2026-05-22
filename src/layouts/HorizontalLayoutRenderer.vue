1d090659ae595468bd25f971ccd743162bf9b005
Nicolas Wang <chaoqing@users.noreply.github.com>
2026-05-22 14:00:27 +0000

    fix flex layout;

--- src/layouts/HorizontalLayoutRenderer.vue
+++ src/layouts/HorizontalLayoutRenderer.vue
@@ -9,7 +9,6 @@
         v-for="(element, index) in (layout.uischema as Layout).elements"
         :key="`${layout.path}-${(layout.uischema as Layout).elements.length}-${index}`"
         :class="styles.horizontalLayout.item"
-        :style="itemStyle"
       >
         <dispatch-renderer
           :schema="layout.schema"
@@ -34,7 +33,7 @@ import {
   useJsonFormsLayout,
   type RendererProps,
 } from '@jsonforms/vue';
-import { computed, defineComponent } from 'vue';
+import { defineComponent } from 'vue';
 import { usePrimeVueLayout } from '../util';
 import Fluid from 'primevue/fluid';
 
@@ -48,18 +47,7 @@ const layoutRenderer = defineComponent({
     ...rendererProps<Layout>(),
   },
   setup(props: RendererProps<Layout>) {
-    const layout = usePrimeVueLayout(useJsonFormsLayout(props));
-    const itemStyle = computed(() => {
-      const count = (props.uischema as Layout).elements.length;
-      return {
-        flex: `1 1 ${100 / count}%`,
-        minWidth: '200px',
-      };
-    });
-    return {
-      ...layout,
-      itemStyle,
-    } as Record<string, any>;
+    return usePrimeVueLayout(useJsonFormsLayout(props));
   },
 }) as any;
 
@@ -78,6 +66,6 @@ export default layoutRenderer;
   display: flex;
   flex: 1;
   flex-direction: column;
-  min-width: 0;
+  min-width: 120px;
 }
 </style>
